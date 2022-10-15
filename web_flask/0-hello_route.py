a8c5b0b63f4b50533db11fe693de879acf32edba
classychizzy <chizobachukwuoma@gmail.com>
2022-10-15 22:43:39 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""an app that uses flask to display text on the browser
+"""
+
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,3 +10,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
     return ' “Hello HBNB!” '
+
+
+"""configures the port and ip of the flask app"""
+app.run(host="0.0.0.0", port=5000)
