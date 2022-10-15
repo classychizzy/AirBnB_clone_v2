bfb19d7b0a2fb20abfb3d3b3a7a50e07e6a08d48
classychizzy <chizobachukwuoma@gmail.com>
2022-10-15 23:18:33 +0000

    Task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,9 +9,14 @@ app = Flask(__name__)
 """
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_HBNB():
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
 
 
 """configures the port and ip of the flask app"""
